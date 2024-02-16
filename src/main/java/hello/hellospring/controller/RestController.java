44498a40089416551a4163efcf1e0c011b09a126
kenryu <jryu1521@gmail.com>
2024-02-16 11:33:11 +0900

    adding react

--- src/main/java/hello/hellospring/controller/RestController.java
+++ src/main/java/hello/hellospring/controller/RestController.java
@@ -1,2 +1,14 @@
-package hello.hellospring.controller;public class RestController {
+package hello.hellospring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @GetMapping("/showMe")
+    public List<String> hello() {
+        return Arrays.asList("첫번째 인사", "두번째 인사");
+    }
+
 }
